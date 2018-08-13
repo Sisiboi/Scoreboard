85c40706a767d52eee22338703823d628b014341
Sisiboi <simon.vanhove0@gmail.com>
2018-08-13 13:46:15 +0200

    updated gulpfile to compile sass and use browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,27 @@
 const gulp = require ('gulp');
 const sass = require('gulp-sass');
-const browerSync = ('browser-sync').create();
+const browserSync = require ('browser-sync').create();
+
+
+// sass compiler
+
+gulp.task('sass', function(){
+ return gulp.src(['src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
+});
+
+// watch & serve
+gulp.task('serve', ['sass'], function(){
+    browserSync.init({
+        server: './src'
+    });
+
+    gulp.watch(['src/scss/*.scss'], ['sass']);
+    gulp.watch(['src/*.html']).on('change', browserSync.reload);
+
+});
+
+// default
+gulp.task('default', ['serve']);
